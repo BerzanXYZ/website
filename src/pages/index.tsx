4220814276c6d08d950441554dbdc63e6892be0f
BerzanXYZ <berzan@tutanota.com>
2022-06-13 21:56:03 +0300

    Add custom font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>Berzan</title>
-        <meta name="description" content="Berzan, builds amazing websites and apps." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <>
+            <Head>
+                <title>Berzan</title>
+                <meta name="description" content="Berzan, builds amazing websites and apps." />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-    </div>
-  )
+        </>
+    )
 }
 
 export default Home
