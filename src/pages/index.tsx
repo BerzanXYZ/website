6fc7483011c51b254e4b485e2e97b2eaca48b4a8
BerzanXYZ <berzan@tutanota.com>
2022-06-13 21:59:11 +0300

    First component created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { Page } from '../components/Page'
 
 const Home: NextPage = () => {
     return (
-        <>
+        <Page>
             <Head>
                 <title>Berzan</title>
                 <meta name="description" content="Berzan, builds amazing websites and apps." />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-
-        </>
+        </Page>
     )
 }
 
