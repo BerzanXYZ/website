036b6ae2184ec3640de93812140cb0310cd9974a
Berzan <berzan@tutanota.com>
2022-06-21 19:46:19 +0300

    Improve card styling

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -4,18 +4,18 @@ export const CardVer = tailt.div`
     flex justify-center items-center
     font-bold text-3xl sm:text-4xl md:text-5xl lg:text:6xl
     rounded-xl h-[calc(25vw+25rem)] sm:h-[calc(30vw+9rem)] max-h-[40rem]
-    shadow-[inset_0_0_0.4rem_#eee]
+    shadow-[inset_0_0_0.4rem_#fff]
     dark:shadow-[inset_0_0_0.4rem_#666]
-    bg-cyan-400 bg-opacity-40 backdrop-blur-lg
-    dark:bg-cyan-900 dark:bg-opacity-40
+    bg-cyan-300 bg-opacity-40 backdrop-blur-lg
+    dark:bg-cyan-800 dark:bg-opacity-40
 `
 
 export const CardHor = tailt.div`
     flex justify-center items-center sm:col-span-2
     font-bold text-3xl sm:text-4xl md:text-5xl lg:text:6xl
     rounded-xl h-[calc(25vw+25rem)] sm:h-[calc(20vw+6rem)] max-h-96
-    shadow-[inset_0_0_0.4rem_#eee]
+    shadow-[inset_0_0_0.4rem_#fff]
     dark:shadow-[inset_0_0_0.4rem_#666]
-    bg-cyan-400 bg-opacity-40 backdrop-blur-lg
-    dark:bg-cyan-900 dark:bg-opacity-40
+    bg-cyan-300 bg-opacity-40 backdrop-blur-lg
+    dark:bg-cyan-800 dark:bg-opacity-40
 `
